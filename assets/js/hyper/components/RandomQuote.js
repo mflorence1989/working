e74a68d920612ba7b3b7009f481de6aa0e66effc
Mario Florence <mflorence1989@gmail.com>
2018-02-15 15:29:06 -0500

    create random quote javascript file

---
+++ assets/js/hyper/components/RandomQuote.js
@@ -0,0 +1,31 @@
+import {h, app} from 'hyperapp'
+
+export default function TopImg({state, actions}) {
+  return (
+  <section id="TopImg">
+  <div class="container">
+  <div class="title">
+  <h5>
+  Welcome
+  </h5>
+  <h1>
+  LEFTOVERS
+  </h1>
+  </div>
+
+  <div class="contact-info">
+  <div class="container">
+  <div class="booking"> Book Table Directly </div>
+  <h2>(718) - 642 - 9542</h2>
+  <div class="hours">
+  Opening Hours <strong> Mon - Fri:</strong> 9am -9pm
+  <strong> Weekend: </strong> 9am -1am
+  </div>
+  </div>
+  </div>
+</div>
+  </section>
+  )
+}
+// <Header state={state} actions={actions}/>
+// <Button state={state} actions={actions}/>
