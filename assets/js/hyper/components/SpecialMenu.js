c02799948cc707d1a1c9fc3a8c31f303a6d32253
Mario Florence <mflorence1989@gmail.com>
2018-02-15 13:56:13 -0500

    add picture and styling to menu

--- assets/js/hyper/components/SpecialMenu.js
+++ assets/js/hyper/components/SpecialMenu.js
@@ -4,22 +4,30 @@ export default function SpecialMenu({state, actions}) {
   return (
   <section id="SpecialMenu">
   <div class="container">
-  <h5></h5>
-  <h2></h2>
+  <h5 class="comp-title">Special Menu</h5>
+  <h2>Delicious Flavour Of Autumn</h2>
   <div class="row">
-  <div class="col-md-4">
 
-  </div>
 
   <div class="col-md-4">
+  <div class="box">
+  <div class="box-img">  <div class="price-circle">$25</div></div>
+
+  <span class="title">Super BBQ Grill NO FUMA </span>
+  <p class="details">Fried eggs, steak, papafritas or baked potatoes, side of vegetables</p>
 
   </div>
+  </div>
+
   <div class="col-md-4">
+  box
+  </div>
 
+  <div class="col-md-4">
+  box
   </div>
   </div>
-<a href="#">View Full Menu</a>
-
+  <a href="#">View Full Menu </a>
   </div>
   </section>
   )
